f7d286af4d29aaee3cb447303411b0b113d8c49f
siam397 <52734646+siam397@users.noreply.github.com>
2020-01-09 17:46:53 +0600

    cssjs

--- templates/todolist.js
+++ templates/todolist.js
@@ -1,16 +1,10 @@
-$("body").on("click","span",function(){
-	$(this).parent().fadeOut("medium",function(){
-		$(this).remove( );
+$("span").on("click",function(){
+	$(this).parent().fadeOut(2000,function(){
+		$(this).remove();
 	})
 })
-//$("body").on("keypress","input",function(event){
-	//if(event.which===13)
-	//{
-	//	var str=$(this).val()
-	//	$(this).val("")
-	//	$("ul").append("<li><span><i class='far fa-trash-alt'></i></span>"+str+"</li>")
-	//}
-})
+
+
 $("i").on("click",function(){
 	$("input").fadeToggle("slow")
 })
